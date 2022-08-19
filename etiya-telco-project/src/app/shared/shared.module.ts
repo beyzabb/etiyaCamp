1fb40ac7afbdf6595e85d8171fd32b6cdc652071
Beyza <bbasaga06@gmail.com>
2022-08-19 14:31:53 +0300

    add side-filter

--- etiya-telco-project/src/app/shared/shared.module.ts
+++ etiya-telco-project/src/app/shared/shared.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
+import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    OverlayTitleComponent
+    OverlayTitleComponent,
+    SideFilterComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[OverlayTitleComponent]
+  exports:[
+    OverlayTitleComponent,
+  SideFilterComponent,
+  NavbarComponent
+  ]
 })
 export class SharedModule { }
