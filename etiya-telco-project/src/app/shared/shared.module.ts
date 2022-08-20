1c8b9d922ea25b0efdf09098d3c653f08f2708e3
Beyza <bbasaga06@gmail.com>
2022-08-20 18:14:55 +0300

    add showcase page

--- etiya-telco-project/src/app/shared/shared.module.ts
+++ etiya-telco-project/src/app/shared/shared.module.ts
@@ -5,6 +5,15 @@ import { SideFilterComponent } from './components/side-filter/side-filter.compon
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InfoTitleComponent } from './components/info-title/info-title.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { SettingsMenuComponent } from './components/settings-menu/settings-menu.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { TableInfoComponent } from './components/table-info/table-info.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ContainerComponent } from './components/container/container.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+import { WarningMessageComponent } from './components/warning-message/warning-message.component';
+import { AddNewItemButtonComponent } from './components/add-new-item-button/add-new-item-button.component';
+import { ShowcaseComponent } from './pages/showcase/showcase.component';
 
 
 
@@ -14,7 +23,16 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
     SideFilterComponent,
     NavbarComponent,
     InfoTitleComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    SettingsMenuComponent,
+    UserInfoComponent,
+    TableInfoComponent,
+    NotFoundComponent,
+    ContainerComponent,
+    ConfirmComponent,
+    WarningMessageComponent,
+    AddNewItemButtonComponent,
+    ShowcaseComponent
   ],
   imports: [
     CommonModule
@@ -24,7 +42,15 @@ import { MainLayoutComponent } from './components/main-layout/main-layout.compon
     SideFilterComponent,
     NavbarComponent,
     InfoTitleComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    SettingsMenuComponent,
+    UserInfoComponent,
+    ContainerComponent,
+    NotFoundComponent,
+    TableInfoComponent,
+    ConfirmComponent,
+    WarningMessageComponent
+    
   ]
 })
 export class SharedModule { }
